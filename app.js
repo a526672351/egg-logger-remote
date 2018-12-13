bba7c239bce9a74c787fa3ac86d9acc5dce9e6a5
huangqicong <huangqicong@wtoip.com>
2018-12-13 16:37:33 +0800

    feat: 支持1.0版本

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const RemoteTransport = require('./lib/remote_transport');
 
 module.exports = (app) => {
-    // app.getLogger('logger').set('remote', new RemoteTransport({ level: 'DEBUG', app }));
-    app.logger.set('remote', new RemoteTransport({ level: 'DEBUG', app }));
-    app.logger.disable('file');
-    app.coreLogger.info('[egg-logger-remote] start logger transport to remote server');
+  // app.getLogger('logger').set('remote', new RemoteTransport({ level: 'DEBUG', app }));
+  app.logger.set('remote', new RemoteTransport({ level: 'DEBUG', app }));
+  app.logger.disable('file');
+  app.coreLogger.info('[egg-logger-remote] start logger transport to remote server');
 };
